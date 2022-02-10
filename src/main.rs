60906e2df91c006b3984560dc8cc087118441fa8
Yukitteru.A <yukitteru.a@gmail.com>
2022-02-10 22:10:52 +0500

    Use palette:Srgb for color.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
 use std::env;
 use std::fs::File;
-use image::codecs::png::PngEncoder;
 use image::ColorType;
+use image::png::PNGEncoder;
+use palette::Pixel;
+use palette::Srgb;
 
-fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
-    let out = File::create(filename);
-    let encoder = PngEncoder::new(out);
-    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Rgb8)?;
-    return Ok(());
+fn write_image(filename: &str, pixels: &[u8], bounds:(usize, usize)) -> Result<(), std::io::Error> {
+    let out = File::create(filename).expect("Unable to open file");
+    let encoder = PNGEncoder::new(out);
+    encoder.encode(pixels, bounds.0 as u32, bounds.1 as u32, ColorType::RGB(8))?;
+    Ok(())
 }
 
 fn render(pixels: &mut [u8], bounds: (usize, usize)) {
     assert_eq!(pixels.len(), bounds.0 * bounds.1 * 3);
 
     for y in 0..bounds.1 {
+        eprintln!("scan lines remaining {}", bounds.1 - y); //scanlines dbg
         for x in 0..bounds.0 {
-            let r = (x as f32 / (bounds.0 as f32 - 1.0)) as f32;
-            let g = (y as f32 / (bounds.1 as f32 - 1.0)) as f32;
-            let b = 0.25;
+            let color = Srgb::new(
+                (x as f32 / (bounds.0 as f32 - 1.0)) as f32,
+                (y as f32 / (bounds.1 as f32 - 1.0)) as f32,
+                0.25,
+            );
             let i = y * bounds.0 + x;
-            pixels[i * 3] = (255.99 * r) as u8;
-            pixels[i * 3 + 1] = (255.99 * g) as u8;
-            pixels[i * 3 + 2] = (255.99 * b) as u8;
+            let pixel: [u8; 3] = color.into_format().into_raw();
+            pixels[i * 3] = pixel[0];
+            pixels[i * 3 + 1] = pixel[1];
+            pixels[i * 3 + 2] = pixel[2];
         }
     }
 }
