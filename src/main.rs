2ad2c1775c7fbe4dff7509f13184a06cb0b223d6
Erik <yukitteru.a@gmail.com>
2022-05-23 15:30:52 +0500

    Multiple samples per pixel

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use image::png::PNGEncoder;
 use image::ColorType;
 use palette::Pixel;
 use palette::Srgb;
+use rand::Rng;
 use std::env;
 use std::f64::MAX;
 use std::fs::File;
@@ -21,19 +22,6 @@ fn write_image(
     Ok(())
 }
 
-fn hit_sphere(center: Point3D, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin - center;
-    let a = r.direction.length_squared();
-    let half_b = oc.dot(&r.direction);
-    let c = oc.length_squared() - radius * radius;
-    let discriminant = half_b * half_b - a * c;
-    return if discriminant < 0.0 {
-        -1.0
-    } else {
-        return (-half_b - discriminant.sqrt()) / a;
-    };
-}
-
 fn ray_color(ray: &Ray, world: &Vec<Sphere>) -> Srgb {
     let hit = hit_world(world, ray, 0.001, std::f64::MAX);
     return match hit {
@@ -44,7 +32,7 @@ fn ray_color(ray: &Ray, world: &Vec<Sphere>) -> Srgb {
                 0.5 * n.x() as f32 + 0.5,
                 0.5 * n.y() as f32 + 0.5,
                 0.5 * n.z() as f32 + 0.5,
-            )
+            );
         }
         None => {
             let t: f32 = 0.5 * (ray.direction.unit_vector().y() as f32 + 1.0);
@@ -54,7 +42,7 @@ fn ray_color(ray: &Ray, world: &Vec<Sphere>) -> Srgb {
                 (1.0 - t) * 1.0 + t * 1.0,
             )
         }
-    }
+    };
 }
 
 #[test]
@@ -68,6 +56,9 @@ fn test_ray_color() {
 
 fn render(pixels: &mut [u8], bounds: (usize, usize)) {
     assert_eq!(pixels.len(), bounds.0 * bounds.1 * 3);
+
+    let samples_per_pixel = 8;
+
     let camera = Camera::new(
         Point3D::new(0.0, 0.0, 0.0),
         2.0,
@@ -77,18 +68,26 @@ fn render(pixels: &mut [u8], bounds: (usize, usize)) {
     let mut world: Vec<Sphere> = Vec::new();
     world.push(Sphere::new(Point3D::new(0.0, 0.0, -1.0), 0.5));
     world.push(Sphere::new(Point3D::new(0.0, -100.5, -1.0), 100.0));
+    let mut rng = rand::thread_rng();
 
     for y in 0..bounds.1 {
+        eprint!(".");
         for x in 0..bounds.0 {
-            eprint!(".");
-            let u = (x as f64) / (bounds.0 as f64 - 1.0);
-            let v = (bounds.1 as f64 - y as f64) / (bounds.1 as f64 - 1.0);
-            let r = Ray::new(
-                camera.origin,
-                camera.lower_left_corner + camera.horizontal * u + camera.vertical * v
-                    - camera.origin,
+            let mut pixel_colors: Vec<f32> = vec![0.0; 3];
+            for _s in 0..samples_per_pixel {
+                let u = (x as f64 + rng.gen::<f64>()) / (bounds.0 as f64 - 1.0);
+                let v = (bounds.1 as f64 - (y as f64 + rng.gen::<f64>())) / (bounds.1 as f64 - 1.0);
+                let r = camera.get_ray(u, v);
+                let c = ray_color(&r, &world);
+                pixel_colors[0] += c.red;
+                pixel_colors[1] += c.green;
+                pixel_colors[2] += c.blue;
+            }
+            let color = Srgb::new(
+                pixel_colors[0] / samples_per_pixel as f32,
+                pixel_colors[1] / samples_per_pixel as f32,
+                pixel_colors[2] / samples_per_pixel as f32,
             );
-            let color = ray_color(&r, &world);
 
             let i = y * bounds.0 + x;
             let pixel: [u8; 3] = color.into_format().into_raw();
